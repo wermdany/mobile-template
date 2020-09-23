ea0433b4c40006c8bfd5bc73f369bea09e5b92d8
zxf <wermdany@163.com>
2020-09-23 17:39:43 +0800

    init

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,28 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
+
+const requireModule = require.context("./modules", false, /\.js$/);
+
+const modules = requireModule.keys().reduce((module, path) => {
+  // ./app.js => app
+  const name = path.replace(/^\.\/(.*)\.\w+$/, "$1");
+  // 如果文件是空的则，下面这句取不出来结果
+  const value = requireModule(path);
+  module[name] = value.default;
+  return module;
+}, {});
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {}
+  modules,
+  plugins: [
+    createPersistedState({
+      key: "vue_store"
+    })
+  ]
 });
